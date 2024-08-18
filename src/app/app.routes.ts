a459c8169107b06b8d77ae10ef938c7ed91275b8
paolaapap <paola.pappalardo@outlook.it>
2024-08-18 10:19:25 +0200

    Paola_login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
+import { HomeComponent } from './componenti/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'registrazione', component: RegistrazioneComponent},
+    {path: '', component: LoginComponent},
+    {path: 'home', component: HomeComponent}
+];
---
+++ src/app/componenti/registrazione/registrazione.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registrazione',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './registrazione.component.html',
+  styleUrl: './registrazione.component.css'
+})
+export class RegistrazioneComponent implements OnInit{
+  regForm!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.regForm = new FormGroup({
+      nome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z ]*$')]), // Solo lettere e spazi
+      matricola: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]), //richiede esattamente 10cifre numeriche 
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8) // Almeno 8 caratteri
+      ])
+    })  
+  }
+
+  onSubmit(){
+    console.log(this.regForm);  
+  }
+
+}
